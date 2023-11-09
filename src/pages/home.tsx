aace65e48d375827230bbead343f1582a08b8c1e
sherzod555 <wiut00007584@gmail.com>
2023-11-09 23:39:37 +0500

    first commit-pc version done

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,10 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+import MoreNFTs from "../compoents/moreNfts";
 
 export const Home = () => {
-  return <Link to="/artist">Home</Link>;
+  return (
+    <>
+      <MoreNFTs/>
+    </>
+  )
 };
