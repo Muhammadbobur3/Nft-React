97344af0f96a5440acfdbf21c68d4be4bd372c9a
Muhammadbobur3 <khamidullayevbobur@gmail.com>
2023-11-10 18:33:58 +0500

    resolve conflict

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,23 +1,13 @@
-<<<<<<< HEAD
-// import { Link } from "react-router-dom";
 import MoreNFTs from "../compoents/moreNfts";
-=======
-import { Link } from "react-router-dom";
 import Collection from "../components/collections";
 import { Banner } from "../components/banner/banner";
->>>>>>> 557413b2bf34205dc04c2878fd0e9412589d2acc
 
 export const Home = () => {
   return (
     <>
-<<<<<<< HEAD
-      <MoreNFTs/>
-    </>
-  )
-=======
       <Banner />
-      <Collection></Collection>
+      <Collection />
+      <MoreNFTs />
     </>
   );
->>>>>>> 557413b2bf34205dc04c2878fd0e9412589d2acc
 };
