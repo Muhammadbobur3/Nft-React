64c757a3b34da863ce748844bc5a1c52d7811031
Dilijan11 <developer7747@gmail.com>
2023-11-10 18:18:57 +0500

    Second Attempt

--- src/components/collections.tsx
+++ src/components/collections.tsx
@@ -14,19 +14,19 @@ import man from "../assets/Man.png";
 
 export const Collection = () => {
   return (
-    <div className=" mx-auto w-8/12 px-28">
+    <div className="mx-auto w-8/12 px-28">
       <div className="w- flex flex-col ">
         <h2 className="text-4xl font-serif text-white mb-1">Trending Collection</h2>
         <p className="text-xl font-serif text-white">Checkout our weekly updated trending collection.</p>
       </div>
-      <div className="flex justify-center gap-6">
+      <div className="flex flex-wrap justify-center gap-6">
         <div className="flex">
           <div className="flex flex-col justify-center mt-14">
-            <img className="w-80" src={dog} alt="image" />
+            <img width={320} src={dog} alt="image" />
             <div className="flex justify-center align-center pt-3 gap-3">
-              <img className="w-23 h-23" src={cat} alt="image" />
-              <img className="w-23 h-23" src={bear} alt="image" />
-              <button className="px-2 text-white text-3xl bg-purple-500 rounded-2xl p-0">1025+</button>
+              <img className="w-25 h-25" src={cat} alt="image" />
+              <img className="w-25 h-25" src={bear} alt="image" />
+              <button className="px-2 text-white text-3xl bg-purple-500 rounded-3xl p-0">1025+</button>
             </div>
             <h2 className="text-xl text-white mt-2">DSGN Animals</h2>
             <a className="flex mt-2" href="#">
@@ -37,7 +37,7 @@ export const Collection = () => {
         </div>
         <div className="flex">
           <div className="flex flex-col justify-center mt-14">
-            <img className="w-80" src={mushroom} alt="image" />
+            <img width={320} src={mushroom} alt="image" />
             <div className="flex justify-center align-center pt-3 gap-3">
               <img className="w-23 h-23" src={mushroom2} alt="image" />
               <img className="w-23 h-23" src={mushroom3} alt="image" />
@@ -52,7 +52,7 @@ export const Collection = () => {
         </div>
         <div className="flex">
           <div className="flex flex-col justify-center mt-14">
-            <img className="w-80" src={disco} alt="image" />
+            <img width={320} src={disco} alt="image" />
             <div className="flex justify-center align-center pt-3 gap-3">
               <img className="w-23 h-23" src={disco2} alt="image" />
               <img className="w-23 h-23" src={disco3} alt="image" />
