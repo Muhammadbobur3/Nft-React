b7ca4baf44c1871519e3dfc82517d964d994d48d
sherzod555 <wiut00007584@gmail.com>
2023-11-11 00:11:45 +0500

    componen file fixed

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,23 +1,13 @@
-<<<<<<< HEAD
-// import { Link } from "react-router-dom";
-import MoreNFTs from "../compoents/moreNfts";
-=======
-import { Link } from "react-router-dom";
+import MoreNFTs from "../components/moreNfts";
 import Collection from "../components/collections";
 import { Banner } from "../components/banner/banner";
->>>>>>> 557413b2bf34205dc04c2878fd0e9412589d2acc
 
 export const Home = () => {
   return (
     <>
-<<<<<<< HEAD
       <MoreNFTs/>
+      <Banner />
+      <Collection />
     </>
   )
-=======
-      <Banner />
-      <Collection></Collection>
-    </>
-  );
->>>>>>> 557413b2bf34205dc04c2878fd0e9412589d2acc
 };
