a6b1187a815fcf6e10fef11ae35d54fe51b2bbdd
sherzod555 <wiut00007584@gmail.com>
2023-11-11 20:12:10 +0500

    i did something, but dont know what i did

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,3 +1,5 @@
+// import { Link } from "react-router-dom";
+import MainCard from "../components/main-card";
 import MoreNFTs from "../components/moreNfts";
 import Collection from "../components/collections";
 import { Banner } from "../components/banner/banner";
@@ -5,9 +7,12 @@ import { Banner } from "../components/banner/banner";
 export const Home = () => {
   return (
     <>
-      <MoreNFTs/>
       <Banner />
       <Collection />
+      <MoreNFTs />
+      <MainCard />
     </>
-  )
+  );
 };
+
+export default Home
